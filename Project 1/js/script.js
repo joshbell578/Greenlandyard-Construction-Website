189eb0d93eb965e3e4084da1ed125bf7bd75e2c1
joshbell578 <loerr578@hsd.k12.or.us>
2023-03-07 09:41:17 -0800

    changing files

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -1,8 +1,3 @@
-var time = new Date();
-console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "</h1>"
-
 function validateEmailAddress(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
