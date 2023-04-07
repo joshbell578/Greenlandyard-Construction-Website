84a4719eaf4167f9c984a7b50dac1e154fcc43bf
joshbell578 <loerr578@hsd.k12.or.us>
2023-04-07 08:52:42 -0700

    Fixing form tag

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validateName(name) {
     var name = $("#name").val();
     if (validate(name)) {
-        $("#results").text(name + " is valid :)");
+        $("#results").text(name);
         $("#results").css("color", "green");
     } else {
         $("#results").text(name + " is not correct, please retry:(");
@@ -12,10 +12,45 @@ function validateName(name) {
 function validateEmailAddress(email) {
     var emailAddress = $("#email").val();
     if (validate(email)) {
-        $("#results").text(emailAddress + " is valid :)");
+        $("#results").text(emailAddress);
         $("#results").css("color", "green");
     } else {
         $("#results").text(emailAddress + " is not correct, please retry:(");
         $("#results").css("color", "red");
     }
+}
+
+function postToUrl(url, params, newWindow) {
+    var form = $('<form>');
+    form.attr('action', url);
+    form.attr('method', 'POST');
+    if(newWindow){ form.attr('target', '_blank'); 
+  }
+
+  var addParam = function(paramName, paramValue) {
+      var input = $('<input type="hidden">');
+      input.attr({ 'id':     paramName,
+                 'name':   paramName,
+                 'value':  paramValue });
+      form.append(input);
+    };
+
+    // Params is an Array.
+    if(params instanceof Array){
+        for(var i=0; i<params.length; i++) {
+            addParam(i, params[i]);
+        }
+    }
+
+    // Params is an Associative array or Object.
+    if(params instanceof Object) {
+        for(var key in params){
+            addParam(key, params[key]);
+        }
+    }
+
+    // Submit the form, then remove it from the page
+    form.appendTo(document.body);
+    form.submit();
+    form.remove();
 }
